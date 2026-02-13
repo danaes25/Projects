bc18dd3ce04558aee7e01d77c1918375c2da1121
Dana Sameh <danasameh44@gmail.com>
2026-02-14 01:28:14 +0200

    Delete RoomLink directory

--- RoomLink/force_https.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Skip redirect for localhost (development)
-if ($_SERVER['HTTP_HOST'] !== 'localhost' && (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === 'off')) {
-    $redirect = "https://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-    header("Location: $redirect");
-    exit();
-}
-?>
